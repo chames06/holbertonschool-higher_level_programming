9cc85952e6676fcd732314312d62c8835414742d
chames06 <oukelul@gmail.com>
2025-11-17 15:27:20 +0100

    2eme

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 
 # Get the last digit (preserving the sign for negative numbers)
-last_digit = int(str(number)[-1])
-if number < 0:
-    last_digit = -last_digit
+last_digit = number % 10
+if number < 0 and last_digit != 0:
+    last_digit = last_digit - 10
 
 # Determine the condition
 if last_digit > 5:

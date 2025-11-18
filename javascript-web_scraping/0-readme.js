58ef640cfb5eaccd30b4fc1eb6306aca9a78689f
chames06 <oukelul@gmail.com>
2025-11-18 09:47:38 +0100

    15eme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,19 +1,14 @@
-#!/usr/bin/node
-// 0-readme.js
-const fs   = require('fs');
-const path = process.argv[2];          // first argument after `node`
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = process.argv[2]
 
 if (!path) {
-  console.error('Usage: node 0-readme.js <file-path>');
-  process.exit(1);
+  console.error('Usage: ./0-readme.js <file-path>')
+  process.exit(1)
 }
 
 fs.readFile(path, 'utf8', (err, data) => {
-  if (err) {
-    /* Print the whole error object – it will include name, code, errno … */
-    console.log(err);
-  } else {
-    /* The file was read successfully: print its content */
-    console.log(data);
-  }
-});
+  if (err) console.log(err)
+  else console.log(data)
+})

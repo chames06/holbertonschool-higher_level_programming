877e7a2c0804e616f3e7c94f8b60a5ae111f45e0
chames06 <oukelul@gmail.com>
2025-11-18 09:41:48 +0100

    15eme

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,10 @@
+/* 0-script.js */
+
+/* Select the <header> tag */
+const header = document.querySelector('header');
+
+/* Make sure we actually found something before trying to style it */
+if (header) {
+  /* Change its text color to red (#FF0000) */
+  header.style.color = '#FF0000';
+}

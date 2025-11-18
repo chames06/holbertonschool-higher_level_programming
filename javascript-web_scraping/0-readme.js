770a853ac0adea45ac3636bdcd6fc17099b76eb7
chames06 <oukelul@gmail.com>
2025-11-18 09:50:36 +0100

    15eme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const fs = require('fs')
-const path = process.argv[2]
+const fs = require('fs');
+const path = process.argv[2];
 
 if (!path) {
-  console.error('Usage: ./0-readme.js <file-path>')
-  process.exit(1)
+  console.error('Usage: ./0-readme.js <file-path>');
+  process.exit(1);
 }
 
 fs.readFile(path, 'utf8', (err, data) => {
-  if (err) console.log(err)
-  else console.log(data)
-})
+  if (err) console.log(err);
+  else console.log(data);
+});
